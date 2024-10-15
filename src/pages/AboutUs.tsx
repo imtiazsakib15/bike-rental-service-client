c2a19058e51707550b68a36e04a1f6157315524b
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-10-15 20:37:17 +0600

    contact info section added in about us page

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,3 +1,4 @@
+import ContactInfo from "@/components/custom/AboutUs/ContactInfo";
 import MissionAndVision from "@/components/custom/AboutUs/MissionAndVision";
 import OurStory from "@/components/custom/AboutUs/OurStory";
 
@@ -6,6 +7,7 @@ const AboutUs = () => {
     <>
       <OurStory />
       <MissionAndVision />
+      <ContactInfo />
     </>
   );
 };
